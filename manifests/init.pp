72d38653ba5322e987b7e87c3ac09f68c65d416c
Alessandro Franceschi <al@lab42.it>
2012-08-06 16:28:29 +0000

    Minimal tartatus module.

---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+class tartarus (
+  base_source         = 'http://wertarbyte.de/tartarus/',
+  version             = '0.9.8',
+  install_destination = '/opt',
+  user                = 'root',
+  group               = 'root',
+  ) {
+
+  puppi::netinstall { 'netinstall_tartarus':
+    url                 => "${tartarus::base_source}/tartarus-${tartarus::version}.tar.bz2",
+    destination_dir     => "${tartarus::install_destination}/" ,
+    extract_command     => 'tar -jxf',
+    postextract_command => "ln -s ${tartarus::install_destination}/tartarus-${tartarus::version} ${tartarus::install_destination}/tartarus",
+    extracted_dir       => "tartarus-${tartarus::version}",
+  }
+
+  file { '/etc/tartarus':
+    ensure => directory,
+    mode   => '0750',
+    owner  => $tartarus::user,
+    group  => $tartarus::group,
+  }
+
+}
---
+++ manifests/job.pp
@@ -0,0 +1,33 @@
+define tartarus::job (
+  directory,
+  storage_method       = 'ftp',
+  storage_ftp_server   = '',
+  storage_ftp_user     = '',
+  storage_ftp_password = '',
+  compression_method   = 'bzip2',
+  schedule             = '',
+  template_file        = 'tartarus/job.conf'
+  ) {
+
+  include tartarus
+
+  file { "/etc/tartarus/${name}.conf":
+    ensure  => present,
+    mode    => '0640',
+    owner   => $tartarus::user,
+    group   => $tartarus::group,
+    content => template($template_file),
+  }
+
+  if ( $schedule ) {
+    file { "tartarus_backup_${name}":
+      path    => "/etc/cron.d/tartarus_backup_${name}",
+      ensure  => $ensure,
+      mode    => '0644',
+      owner   => "root",
+      group   => "root",
+      content => template("tartarus/cron.erb"),
+    }
+  }
+
+}
